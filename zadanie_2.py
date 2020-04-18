264a965a6f02cd34e818bae836020f568878d3c7
Wiktor Gabrys <gabrys.wiktor@gmail.com>
2020-04-18 16:27:55 +0200

    Init code

--- zadanie_2.py
+++ zadanie_2.py
@@ -1,4 +1,17 @@
+import string
+
+print("\n Zadanie 1 \n")
 lista_zakupow = {
     "piekarnia" : ["chleb", "bułki", "masło"],
     "warzywniak" : ["marchew", "seler", "rukola"]
 }
+
+print("Lista zakupów")
+
+for sklep, produkty in lista_zakupow.items():
+    print("Idę do", sklep.capitalize(), "i kupuję tam:", produkty)
+count = 0
+for produkt in lista_zakupow: 
+    if isinstance(lista_zakupow[produkt], list): 
+        count += len(lista_zakupow[produkt]) 
+print("W sumie kupuję", count, "produktów")
