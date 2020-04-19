1a59299c7a0a2354d998c6aa615d403da0dee0cc
Wiktor Gabrys <gabrys.wiktor@gmail.com>
2020-04-19 23:01:13 +0200

    Add zadanie_2

--- zadanie_2.py
+++ zadanie_2.py
@@ -1,1 +1,18 @@
-#siema, pozdrawiam
+import string
+
+print("\n Zadanie 1 \n")
+lista_zakupow = {
+    "piekarnia" : ["chleb", "bułki", "masło"],
+    "warzywniak" : ["marchew", "seler", "rukola"]
+}
+
+print("Lista zakupów")
+
+suma = 0
+for sklep, produkty in lista_zakupow.items():
+    produkty = [produkt.capitalize() for produkt in produkty]
+    print("Idę do", sklep.capitalize(), "i kupuję tam:", produkty)
+    #Prostsze policzenie sumy produktów
+    count = len(produkty)
+    suma = suma + count
+print("W sumie kupuję", suma, "produktów")
